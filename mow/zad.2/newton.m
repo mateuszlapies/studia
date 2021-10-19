8a41e65cbafc7bac4d5ddaf800053cef9348e9c0
Mateox <mat.lapies@gmail.com>
2021-10-19 22:05:59 +0200

    mow & ztp

---
+++ mow/zad.2/newton.m
@@ -0,0 +1,12 @@
+f=input("podaj wzor f(x)=","s");
+df=input("podaj wzor na df/dx=","ss");
+x=input("podaj x0=");
+eps=input("podaj eps=");
+del=1000000;
+while del > eps
+x1=x.-eval(f)./eval(df);
+del=abs(x1.-x)
+x=x1;
+end
+format long
+x=x
