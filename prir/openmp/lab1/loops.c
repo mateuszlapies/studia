d021b9f9f22a7a1383fbfdf1aa53c9ea20a16f30
mateuszlapies <mat.lapies@gmail.com>
2022-03-15 19:33:05 +0100

    lab-1 point 4

--- prir/openmp/lab1/loops.c
+++ prir/openmp/lab1/loops.c
@@ -2,34 +2,42 @@
 #include <stdio.h>
 
 void strategy_a(int iterations) {
-    int i;
+    int i; int sum = 0;
     #pragma omp parallel for schedule(static,3)
-    for(i=0;i<iterations;i++){ printf("thread %d, index %d\n",omp_get_thread_num(),i); }
+    for(i=0;i<iterations;i++){ sum++; }
 }
 
 void strategy_b(int iterations) {
-    int i;
+    int i; int sum = 0;
     #pragma omp parallel for schedule(static)
-    for(i=0;i<iterations;i++){ printf("thread %d, index %d\n",omp_get_thread_num(),i); }
+    for(i=0;i<iterations;i++){ sum++; }
 }
 
 void strategy_c(int iterations) {
-    int i;
+    int i; int sum = 0;
     #pragma omp parallel for schedule(dynamic,3)
-    for(i=0;i<iterations;i++){ printf("thread %d, index %d\n",omp_get_thread_num(),i); }
+    for(i=0;i<iterations;i++){ sum++; }
 }
 
 void strategy_d(int iterations) {
-    int i;
+    int i; int sum = 0;
     #pragma omp parallel for schedule(dynamic)
-    for(i=0;i<(iterations);i++){ printf("thread %d, index %d\n",omp_get_thread_num(),i); }
+    for(i=0;i<(iterations);i++){ sum++; }
 }
 
 int main() {
-    int iterations = 15;
+    int iterations = 150000;
     omp_set_num_threads(4);
+    double start_time = omp_get_wtime();
     strategy_a(iterations);
+    printf("a: %f\n", omp_get_wtime() - start_time);
+    start_time = omp_get_wtime();
     strategy_b(iterations);
+    printf("b: %f\n", omp_get_wtime() - start_time);
+    start_time = omp_get_wtime();
     strategy_c(iterations);
+    printf("c: %f\n", omp_get_wtime() - start_time);
+    start_time = omp_get_wtime();
     strategy_d(iterations);
+    printf("d: %f\n", omp_get_wtime() - start_time);
 }
