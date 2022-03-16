8e3765cc6fa1d3098240ea2d6449c667d047fad6
mateuszlapies <mat.lapies@gmail.com>
2022-03-16 14:58:47 +0100

    lab-1 point 9

--- prir/openmp/lab1/matrix.cpp
+++ prir/openmp/lab1/matrix.cpp
@@ -7,49 +7,56 @@ int main() {
     omp_set_num_threads(4);
     srand(time(NULL));
 
-    int N = 4;
-    int M = 3;
-    int P = 3;
+    int N = 10;
+    int M = 8;
+    int P = 4;
+
+    printf("N: %i, M: %i, P: %i\n\n", N, M, P);
 
     int main [N][M];
     int multi [M][P];
-    int output [N][P];
+    int output1 [N][P];
+    int output2 [N][P];
 
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < M; j++) {
             main[i][j] = rand()%10;
-            printf("%i\t", main[i][j]);
         }
         for(int p = 0; p < P; p++) {
-            output[i][p] = 0;
+            output1[i][p] = 0;
+            output2[i][p] = 0;
         }
-        printf("\n");
     }
-    printf("\n");
     for(int i = 0; i < M; i++) {
         for(int j = 0; j < P; j++) {
             multi[i][j] = rand()%10;
-            printf("%i\t", multi[i][j]);
         }
-        printf("\n");
     }
-    printf("\n");
-    double time = omp_get_wtime();
     int i,j,k;
-    #pragma omp parallel for private(i)
     for(i = 0; i < N; i++) {
         for(j = 0; j < P; j++) {
             for(k = 0; k < M; k++) {
-                output[i][j] += main[i][k] * multi[k][j];
+                output1[i][j] += main[i][k] * multi[k][j];
+            }
+        }
+    }
+    double time = omp_get_wtime();
+    #pragma omp parallel for private(i) schedule(dynamic)
+    for(i = 0; i < N; i++) {
+        for(j = 0; j < P; j++) {
+            for(k = 0; k < M; k++) {
+                output2[i][j] += main[i][k] * multi[k][j];
             }
         }
     }
     printf("time: %f\n\n", omp_get_wtime() - time);
 
+    bool correct = true;
     for(i = 0; i < N; i++) {
         for(j = 0; j < P; j++) {
-            printf("%i\t", output[i][j]);
+            if(output1[i][j] != output2[i][j])
+                correct = false;
         }
-        printf("\n");
     }
+    printf("is correct? %s", correct ? "OK" : "NOT OK");
 }
